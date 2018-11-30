2b1220fe15b989e44d1be6be67d68b519b9ea54c
Florian Eck <it-support@friends-systems.de>
2018-11-30 15:28:50 +0100

    restructure form for showing defaults

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :epm_better_create_issue do
   name 'Epm BetterCreateIssue'
   author 'Florian Eck for akquinet'
-  description 'Provide better form functionalit for the built in EpmIssuesCreateNew page module'
+  description 'Provide better form functionality for the built in EpmIssuesCreateNew page module'
   version '1.0'
 end
